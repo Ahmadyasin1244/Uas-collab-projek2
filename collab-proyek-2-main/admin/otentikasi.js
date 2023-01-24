f43f48ced879cda240a913b03d54bd8ae7389a81
Ahmadyasin1244 <ahmad.yasin1244@gmail.com>
2023-01-24 09:53:06 +0700

    projek uas

---
+++ collab-proyek-2-main/admin/otentikasi.js
@@ -0,0 +1,15 @@
+function login() {
+    event.preventDefault();
+
+    let uname = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if (uname == "admin@admin.com" && pass == "escadmin") {
+
+        alert("Selamat Datang Admin!");
+        location.replace("index.html");
+    } else {
+        alert("Selamat Datang!");
+        location.replace("../index.html");
+    }
+}
